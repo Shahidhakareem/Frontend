8db575c4d53434653fd2658d6bec7121e75ab36d
Shahidhakareem <shahidhakareem134@gmail.com>
2025-11-17 01:03:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // base: process.env.VITE_BASE_PATH || "/WebWorld"
-   build: {
-     outDir: "dist"
-   }
-});
+  define: {
+    'process.env': {} // Optional if using environment variables
+  }
+})
