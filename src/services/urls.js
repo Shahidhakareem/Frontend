25ba05faa602ab8980d83af2ceb8bf1a947224c9
Shahidhakareem <shahidhakareem134@gmail.com>
2025-11-17 01:01:59 +0530

    Update urls.js

--- src/services/urls.js
+++ src/services/urls.js
@@ -1,16 +1,15 @@
-const BASE_URL = import.meta.env.VITE_API_BASE || "http://localhost:3001";
-
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3001";
 
 const urls = {
   baseURL: {
     dev: "http://localhost:3001",
-    prod: "https://your-live-api.com",
+    prod: "https://your-backend-service.onrender.com", // Render backend URL
   },
   signUp: "/user/signup",
   login: "/user/login",
   courseList: "/courses",
   addCourse: "/courses/addcourse",
-  addFreeCourse : "courses/addfreecourse"
+  addFreeCourse: "/courses/addfreecourse",
 };
 
 export default urls;
