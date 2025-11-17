5a4076d0fb589be0cee49e72fe3f80d80c27aaa3
Shahidhakareem <shahidhakareem134@gmail.com>
2025-11-17 09:39:37 +0530

    Update urls.js

--- src/services/urls.js
+++ src/services/urls.js
@@ -3,13 +3,13 @@ const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3001";
 const urls = {
   baseURL: {
     dev: "http://localhost:3001",
-    prod: "https://backend-wwgs.onrender.com", // Render backend URL
+    prod: BASE_URL, // use environment variable in deployment
   },
   signUp: "/user/signup",
   login: "/user/login",
   courseList: "/courses",
   addCourse: "/courses/addcourse",
-  addFreeCourse: "/courses/addfreecourse",
+  addFreeCourse: "/courses/addfreecourse"
 };
 
 export default urls;
